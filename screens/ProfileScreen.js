d4e1c2bf86b8e8976815ab6a20e5b24e92c71a7b
Rachel Lee <leerache@wharton.upenn.edu>
2023-11-11 02:00:20 -0500

    profile & map frontend

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,28 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, Image, StyleSheet, TouchableOpacity, Switch} from 'react-native'
 
 const ProfileScreen = () => {
 
-    // const [isAvaliable, setIsAvaliable] = useState(false);
-    // const userName = 'hardCode';
+    const [isAvailable, setIsAvailable] = useState(false);
+    const userName = '[Name]';
+
+    const tagsData = [
+        {key: 'studying', label: 'Studying', active: false },
+        { key: 'eating', label: 'Eating', active: false},
+        { key: 'workingOut', label:'Working Out', active: false},
+    ];
+
+    const [tags, setTags] = useState(tagsData);
+
+    const handleTagToggle = (tagKey) => {
+        setTags((prevTags) => 
+        prevTags.map((tag) => 
+        tag.key === tagKey ? {...tag, active: !tag.active } : tag));
+    }
+
+    const renderActiveTags = () => {
+        return (
+            <View style={styles.tagContainer}> 
+            <Text style={styles.tagText}> Currently: {tags.filter((tag) => tag.active).map((tag) => tag.label).join(', ')}</Text>
+            </View>
+        );
+    };
+
+    const renderAvailableSettings = () => {
+        if (isAvailable) {
+            return (
+                <View style={styles.availableSettings}>
+
+                    {renderActiveTags()}
+
+                    <TouchableOpacity 
+                        style={styles.button} 
+                        onPress = {() => handleTagToggle('studying')}
+                        activeOpacity={0.8}
+                    >
+                        <Text>Studying</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.button} onPress = {() => handleTagToggle('eating')}>
+                        <Text>Eating</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.button} onPress = {() => handleTagToggle('workingOut')}>
+                        <Text>Working Out</Text>
+                    </TouchableOpacity>
+
+                </View>
+            );
+        }
+    };
+
+
 
     return (
-        <View>
-            <Text> 
-                Profile screen lalalala
-            </Text>
+        <View style={styles.container}>
+            <View style={styles.topContainer}>
+                <Text style={styles.greetingText}>
+                    Hi, { userName }!
+                </Text>
+            </View>
+            <View style={styles.bottomContainer}>
+                <Text style={styles.profileText}> 
+                        Status: {isAvailable ? 'Available 😎' : 'Busy 😴'}
+                </Text>
+                <Switch
+                    value={isAvailable}
+                    onValueChange={(value) => setIsAvailable(value)}
+                    style={styles.statusToggle}
+                />
+            
+                {isAvailable && 
+                    renderAvailableSettings()
+                }
+            </View>
         </View>
-    )
-}
+    );
+};
+
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundcolor: 'white',
+        backgroundColor: '#d3d3d3',
         },
     topContainer: {
         flex: 1,
-        backgroundcolor: 'd3d3d3',
+        backgroundColor: 'white',
         alignItems: 'center',
         justifyContent: 'center',
         padding: 16,
@@ -30,13 +100,50 @@ const styles = StyleSheet.create({
     bottomContainer: {
         flex: 2,
         padding: 16,
+        alignItems: 'center',
+        marginTop: 20,
+
     },
     profileImage: {
         width: 150,
         height: 150,
         borderRadius: 75,
         marginBottom: 16,
-    }
-})
+    },
+    greetingText: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginTop: 8,
+    },
+    profileText: {
+        fontSize: 20,
+    },
+    statusToggle: {
+        marginTop: 16,
+        marginBottom: 16,
+        transform: [{ scaleX: 1.3 }, { scaleY: 1.25}],
+    },
+
+    availableSettings: {
+        marginTop: 16, 
+        alignItems: 'center',
+    },
+    button: {
+        backgroundColor: 'lightblue',
+        padding: 8,
+        marginVertical: 4,
+    },
+    activeButton: {
+        backgroundColor: 'blue',
+    },
+    tagContainer: {
+        backgroundColor: 'lightgreen',
+        padding: 8,
+        marginVertical: 4,
+    },
+    tagText: {
+        fontSize: 16,
+    },
+});
 
 export default ProfileScreen;
